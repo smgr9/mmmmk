c0ed8387d240d739e80d7504641a0b29a09c3af3
smgr9 <30799257+smgr9@users.noreply.github.com>
2018-05-13 16:45:34 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+
+function myFunction() {
+                  window.open("lessons.html");}
+
+function myFunctionww() {
+                  window.open("https://www.facebook.com/groups/1040881286080259/learning_content");}
+
+function myFunctionw() {
+                  window.open("pdf/p1.pdf");}
+
+
+
+
+$(document).ready(function(){
+   $('.gg').click(function(){
+       $(".oo").toggle();
+     
+   });
+});
+
+
+
+
+$(document).ready(function(){
+   $('.ll').click(function(){
+       $(".kk").toggle();
+     
+   });
+});
+
+
+
